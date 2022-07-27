0caf078b1a32bc60f7728df479a44eaa8d0be000
Caio Peternela <caiopeternela.dev@gmail.com>
2022-07-27 18:04:52 -0300

    editing the api code

--- jsonflix/urls.py
+++ jsonflix/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('v1/api/', views.api)
 ]
--- jsonflix/views.py
+++ jsonflix/views.py
@@ -1,5 +1,40 @@
 from django.shortcuts import render
+from .models import Netflix
+from django.http import HttpResponse
+import json
 
 
 def home(request):
     return render(request, 'jsonflix/home.html')
+
+
+def api(request):
+
+    type = request.GET.get("type")
+    title = request.GET.get("title")
+
+    qs = Netflix.objects.all()
+    if type:
+        qs = qs.filter(type__contains=type)
+
+    if title:
+        qs = qs.filter(title__contains=title)
+
+    dict = [
+        {
+         "show_id": content.show_id,
+         "type": content.type,
+         "title": content.title,
+         "director": content.director,
+         "cast": content.cast,
+         "country": content.country,
+         "date_added": content.date_added,
+         "release_year": content.release_year,
+         "rating": content.rating,
+         "duration": content.duration,
+         "genres": content.genres,
+         "description": content.description,
+        }
+        for content in qs
+    ]
+    return HttpResponse(json.dumps(dict, ensure_ascii=False, indent=2), content_type="application/json")
