cb2e37027e31b7a0d7266824b16917a98d41b926
Caio Peternela <caiopeternela.dev@gmail.com>
2022-08-04 10:02:27 -0300

    testing filter request with keywords

--- jsonflix/tests/test_true.py
+++ jsonflix/tests/test_true.py
@@ -1,2 +1,8 @@
-def test_in():
-    assert 1 == 1
+import requests
+
+
+def test_filter_with_keyword():
+    response = requests.get("http://127.0.0.1:8000/api/v1/?type=movie&description=Garfield,superheroes")
+    response_body = response.json()
+    for field in response_body:
+        assert field["title"] == "Garfield's Pet Force"
