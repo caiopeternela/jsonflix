acdcc461482c9e3e56ff469665370b222d996938
Caio Peternela <caiopeternela.dev@gmail.com>
2022-08-04 14:57:12 -0300

    testing the country code converter feature

--- jsonflix/tests/test_api.py
+++ jsonflix/tests/test_api.py
@@ -105,3 +105,59 @@ def test_shows_limit(client, db):
     resp = client.get(reverse('api'), dados).json()
 
     assert len(resp) == 1
+
+
+def test_country_code(client, db):
+    Netflix.objects.create(
+        id=22,
+        type="TV Show",
+        title="Resurrection: Ertugrul",
+        director="",
+        cast="Engin Altan Düzyatan, Serdar Gökhan, Hülya Darcan, Kaan Taşaner, Esra Bilgiç, Osman Soykut, Serdar Deniz, Cengiz Coşkun, Reshad Strik, Hande Subaşı",
+        country="Turkey",
+        date_added="2021-09-22",
+        release_year="2018",
+        rating="TV-14",
+        duration="5 Seasons",
+        genres="International TV Shows, TV Action & Adventure, TV Dramas",
+        description="When a good deed unwittingly endangers his clan, a 13th-century Turkish warrior agrees to fight a sultan's enemies in exchange for new tribal land."
+    )
+    Netflix.objects.create(
+        id=2,
+        type="TV Show",
+        title="Blood & Water",
+        director="",
+        cast='''"Ama Qamata, Khosi Ngema, Gail Mabalane, Thabang Molaba, Dillon Windvogel,
+        Natasha Thahane, Arno Greeff, Xolile Tshabalala, Getmore Sithole, Cindy Mahlangu,
+        Ryle De Morny, Greteli Fincham, Sello Maake Ka-Ncube, Odwa Gwanya, Mekaila Mathys,
+        Sandi Schultz, Duane Williams, Shamilla Miller, Patrick Mofokeng"''',
+        country="South Africa",
+        date_added="2021-09-24",
+        release_year="2021",
+        rating="TV-MA",
+        duration="2 Seasons",
+        genres="International TV Shows, TV Dramas, TV Mysteries",
+        description="After crossing paths at a party, a Cape Town teen sets out to prove whether a private-school swimming star is her sister who was abducted at birth."
+    )
+
+    data = {
+        'country': ['TR']
+    }
+    response = client.get(reverse('api'), data).json()
+
+    assert response == [
+        {
+            "id": 22,
+            "type": "TV Show",
+            "title": "Resurrection: Ertugrul",
+            "director": "",
+            "cast": "Engin Altan Düzyatan, Serdar Gökhan, Hülya Darcan, Kaan Taşaner, Esra Bilgiç, Osman Soykut, Serdar Deniz, Cengiz Coşkun, Reshad Strik, Hande Subaşı",
+            "country": "Turkey",
+            "date_added": "2021-09-22",
+            "release_year": "2018",
+            "rating": "TV-14",
+            "duration": "5 Seasons",
+            "genres": "International TV Shows, TV Action & Adventure, TV Dramas",
+            "description": "When a good deed unwittingly endangers his clan, a 13th-century Turkish warrior agrees to fight a sultan's enemies in exchange for new tribal land."
+        }
+    ]
