ca4fd150860b831e09a4140e5889febcdd8972c6
Caio Peternela <caiopeternela.dev@gmail.com>
2022-08-04 10:12:30 -0300

    configuring codecov

--- jsonflix/tests/test_filter_with_keywords.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def test_filter_with_keywords():
-    response = requests.get("http://127.0.0.1:8000/api/v1/?type=movie&description=Garfield,superheroes")
-    response_body = response.json()
-    for field in response_body:
-        assert field["title"] == "Garfield's Pet Force"
