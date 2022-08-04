d742c63a301228ac8f12054eb1cb8f8f47e7ffb0
Caio Peternela <caiopeternela.dev@gmail.com>
2022-08-04 10:06:49 -0300

    testing filter request with keywords

--- jsonflix/tests/test_filter_with_keywords.py
+++ jsonflix/tests/test_filter_with_keywords.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def test_filter_with_keyword():
+def test_filter_with_keywords():
     response = requests.get("http://127.0.0.1:8000/api/v1/?type=movie&description=Garfield,superheroes")
     response_body = response.json()
     for field in response_body:
