eb08feba86208e597dc07f433f2924cbd8f04a9b
Victor Magalhaes <victor.barreto@buser.com.br>
2022-08-05 16:19:11 -0300

    shows creation fixture

--- jsonflix/tests/test_api.py
+++ jsonflix/tests/test_api.py
@@ -1,5 +1,45 @@
+import pytest
 from django.urls import reverse
 from jsonflix.models import Netflix
+
+
+@pytest.fixture
+def shows(db):
+    return Netflix.objects.bulk_create(
+        [
+            Netflix(
+                id=22,
+                type="TV Show",
+                title="Resurrection: Ertugrul",
+                director="",
+                cast="Engin Altan Düzyatan, Serdar Gökhan, Hülya Darcan, Kaan Taşaner, Esra Bilgiç, Osman Soykut, Serdar Deniz, Cengiz Coşkun, Reshad Strik, Hande Subaşı",
+                country="Turkey",
+                date_added="2021-09-22",
+                release_year="2018",
+                rating="TV-14",
+                duration="5 Seasons",
+                genres="International TV Shows, TV Action & Adventure, TV Dramas",
+                description="When a good deed unwittingly endangers his clan, a 13th-century Turkish warrior agrees to fight a sultan's enemies in exchange for new tribal land."
+            ),
+            Netflix(
+                id=2,
+                type="TV Show",
+                title="Blood & Water",
+                director="",
+                cast='''"Ama Qamata, Khosi Ngema, Gail Mabalane, Thabang Molaba, Dillon Windvogel,
+        Natasha Thahane, Arno Greeff, Xolile Tshabalala, Getmore Sithole, Cindy Mahlangu,
+        Ryle De Morny, Greteli Fincham, Sello Maake Ka-Ncube, Odwa Gwanya, Mekaila Mathys,
+        Sandi Schultz, Duane Williams, Shamilla Miller, Patrick Mofokeng"''',
+                country="South Africa",
+                date_added="2021-09-24",
+                release_year="2021",
+                rating="TV-MA",
+                duration="2 Seasons",
+                genres="International TV Shows, TV Dramas, TV Mysteries",
+                description="After crossing paths at a party, a Cape Town teen sets out to prove whether a private-school swimming star is her sister who was abducted at birth."
+            )
+        ]
+    )
 
 
 def test_shows_list(client, db):
@@ -7,39 +47,7 @@ def test_shows_list(client, db):
     assert response.status_code == 200
 
 
-def test_list_shows_description(client, db):
-    Netflix.objects.create(
-        id=22,
-        type="TV Show",
-        title="Resurrection: Ertugrul",
-        director="",
-        cast="Engin Altan Düzyatan, Serdar Gökhan, Hülya Darcan, Kaan Taşaner, Esra Bilgiç, Osman Soykut, Serdar Deniz, Cengiz Coşkun, Reshad Strik, Hande Subaşı",
-        country="Turkey",
-        date_added="2021-09-22",
-        release_year="2018",
-        rating="TV-14",
-        duration="5 Seasons",
-        genres="International TV Shows, TV Action & Adventure, TV Dramas",
-        description="When a good deed unwittingly endangers his clan, a 13th-century Turkish warrior agrees to fight a sultan's enemies in exchange for new tribal land."
-    )
-    Netflix.objects.create(
-        id=2,
-        type="TV Show",
-        title="Blood & Water",
-        director="",
-        cast='''"Ama Qamata, Khosi Ngema, Gail Mabalane, Thabang Molaba, Dillon Windvogel,
-        Natasha Thahane, Arno Greeff, Xolile Tshabalala, Getmore Sithole, Cindy Mahlangu,
-        Ryle De Morny, Greteli Fincham, Sello Maake Ka-Ncube, Odwa Gwanya, Mekaila Mathys,
-        Sandi Schultz, Duane Williams, Shamilla Miller, Patrick Mofokeng"''',
-        country="South Africa",
-        date_added="2021-09-24",
-        release_year="2021",
-        rating="TV-MA",
-        duration="2 Seasons",
-        genres="International TV Shows, TV Dramas, TV Mysteries",
-        description="After crossing paths at a party, a Cape Town teen sets out to prove whether a private-school swimming star is her sister who was abducted at birth."
-    )
-
+def test_list_shows_description(client, shows):
     dados = {
         'description': ['fight,clan,warrior']
     }
@@ -63,36 +71,7 @@ def test_list_shows_description(client, db):
     ]
 
 
-def test_shows_limit(client, db):
-    Netflix.objects.create(
-        id=22,
-        type="TV Show",
-        title="Resurrection: Ertugrul",
-        director="",
-        cast="Engin Altan Düzyatan, Serdar Gökhan, Hülya Darcan, Kaan Taşaner, Esra Bilgiç, Osman Soykut, Serdar Deniz, Cengiz Coşkun, Reshad Strik, Hande Subaşı",
-        country="Turkey",
-        date_added="2021-09-22",
-        release_year="2018",
-        rating="TV-14",
-        duration="5 Seasons",
-        genres="International TV Shows, TV Action & Adventure, TV Dramas",
-        description="When a good deed unwittingly endangers his clan, a 13th-century Turkish warrior agrees to fight a sultan's enemies in exchange for new tribal land."
-    )
-    Netflix.objects.create(
-        id=2,
-        type="TV Show",
-        title="Blood & Water",
-        director="",
-        cast="Ama Qamata, Khosi Ngema, Gail Mabalane, Thabang Molaba, Dillon Windvogel, Natasha Thahane, Arno Greeff, Xolile Tshabalala, Getmore Sithole, Cindy Mahlangu, Ryle De Morny, Greteli Fincham, Sello Maake Ka-Ncube, Odwa Gwanya, Mekaila Mathys, Sandi Schultz, Duane Williams, Shamilla Miller, Patrick Mofokeng",
-        country="South Africa",
-        date_added="2021-09-24",
-        release_year="2021",
-        rating="TV-MA",
-        duration="2 Seasons",
-        genres="International TV Shows, TV Dramas, TV Mysteries",
-        description="After crossing paths at a party, a Cape Town teen sets out to prove whether a private-school swimming star is her sister who was abducted at birth."
-    )
-
+def test_shows_limit(client, shows):
     dados = {
         'limit': [1]
     }
@@ -101,44 +80,11 @@ def test_shows_limit(client, db):
     assert len(resp) == 1
 
 
-def test_country_code(client, db):
-    Netflix.objects.create(
-        id=22,
-        type="TV Show",
-        title="Resurrection: Ertugrul",
-        director="",
-        cast="Engin Altan Düzyatan, Serdar Gökhan, Hülya Darcan, Kaan Taşaner, Esra Bilgiç, Osman Soykut, Serdar Deniz, Cengiz Coşkun, Reshad Strik, Hande Subaşı",
-        country="Turkey",
-        date_added="2021-09-22",
-        release_year="2018",
-        rating="TV-14",
-        duration="5 Seasons",
-        genres="International TV Shows, TV Action & Adventure, TV Dramas",
-        description="When a good deed unwittingly endangers his clan, a 13th-century Turkish warrior agrees to fight a sultan's enemies in exchange for new tribal land."
-    )
-    Netflix.objects.create(
-        id=2,
-        type="TV Show",
-        title="Blood & Water",
-        director="",
-        cast='''"Ama Qamata, Khosi Ngema, Gail Mabalane, Thabang Molaba, Dillon Windvogel,
-        Natasha Thahane, Arno Greeff, Xolile Tshabalala, Getmore Sithole, Cindy Mahlangu,
-        Ryle De Morny, Greteli Fincham, Sello Maake Ka-Ncube, Odwa Gwanya, Mekaila Mathys,
-        Sandi Schultz, Duane Williams, Shamilla Miller, Patrick Mofokeng"''',
-        country="South Africa",
-        date_added="2021-09-24",
-        release_year="2021",
-        rating="TV-MA",
-        duration="2 Seasons",
-        genres="International TV Shows, TV Dramas, TV Mysteries",
-        description="After crossing paths at a party, a Cape Town teen sets out to prove whether a private-school swimming star is her sister who was abducted at birth."
-    )
-
+def test_country_code(client, db, shows):
     data = {
         'country': ['TR']
     }
     response = client.get(reverse('api'), data).json()
-
     assert response == [
         {
             "id": 22,
