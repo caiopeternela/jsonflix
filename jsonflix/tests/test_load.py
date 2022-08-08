4764e55931fdebbe7d5f77b1a0320f07077c2ddc
Caio Peternela <caiopeternela.dev@gmail.com>
2022-08-08 10:51:05 -0300

    adding web ui for load test

--- jsonflix/tests/test_load.py
+++ jsonflix/tests/test_load.py
@@ -1,43 +1,43 @@
-# import gevent
-# from locust import HttpUser, task, between
-# from locust.env import Environment
-# from locust.stats import stats_printer, stats_history
-# from locust.log import setup_logging
-
-# setup_logging("INFO", None)
+import gevent
+from locust import HttpUser, task, between
+from locust.env import Environment
+from locust.stats import stats_printer, stats_history
+from locust.log import setup_logging
 
 
-# class User(HttpUser):
-#     wait_time = between(1, 5)
-#     host = 'https://jsonflix.herokuapp.com'
-
-#     @task
-#     def all_page(self):
-#         self.client.get(url='/api/v1/all')
-
-#     @task
-#     def docs_page(self):
-#         self.client.get(url='/api/v1/docs')
-
-#     @task
-#     def search_page(self):
-#         self.client.get(url='/api/v1/?type=tv_show&country=kr&description=mafia')
-#         self.client.get(url='/api/v1/?type=movie&country=br')
-#         self.client.get(url='/api/v1/?type=movie&country=us&description=dog,cat')
-#         self.client.get(url='/api/v1/?type=tv_show&country=fr')
-#         self.client.get(url='/api/v1/?type=tv_show&country=es&description=crime')
+setup_logging("INFO", None)
 
 
-# # def test_load_locust():
-# env = Environment(user_classes=[User])
-# env.create_local_runner()
-# gevent.spawn(stats_printer(env.stats))
-# gevent.spawn(stats_history, env.runner)
-# env.runner.start(10, spawn_rate=10)
-# gevent.spawn_later(60, lambda: env.runner.quit())
-# env.runner.greenlet.join()
-# failures = env.stats.total.num_failures
+class User(HttpUser):
+    wait_time = between(1, 5)
+    host = 'https://jsonflix.herokuapp.com'
+
+    @task
+    def all_page(self):
+        self.client.get(url='/api/v1/all')
+
+    @task
+    def docs_page(self):
+        self.client.get(url='/api/v1/docs')
+
+    @task
+    def search_page(self):
+        self.client.get(url='/api/v1/?type=tv_show&country=kr&description=mafia')
+        self.client.get(url='/api/v1/?type=movie&country=br')
+        self.client.get(url='/api/v1/?type=movie&country=us&description=dog,cat')
+        self.client.get(url='/api/v1/?type=tv_show&country=fr')
+        self.client.get(url='/api/v1/?type=tv_show&country=es&description=crime')
 
 
-# def test_load_lucost():
-#     assert failures == 0
+def test_load_locust():
+    env = Environment(user_classes=[User])
+    env.create_local_runner()
+    env.create_web_ui("127.0.0.1", 8089)
+    gevent.spawn(stats_printer(env.stats))
+    gevent.spawn(stats_history, env.runner)
+    env.runner.start(10, spawn_rate=10)
+    gevent.spawn_later(60, lambda: env.runner.quit())
+    env.runner.greenlet.join()
+    env.web_ui.stop()
+    failures = env.stats.total.num_failures
+    assert failures == 0
